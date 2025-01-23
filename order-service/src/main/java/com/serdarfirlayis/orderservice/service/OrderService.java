6345c29bb6f527c41e1b571edfb64239f02ec159
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2025-01-23 15:08:44 +0300

    :sparkles: feat: Added Apache Kafka Broker

--- order-service/src/main/java/com/serdarfirlayis/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/serdarfirlayis/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package com.serdarfirlayis.orderservice.service;
 import com.serdarfirlayis.orderservice.dto.InventoryResponse;
 import com.serdarfirlayis.orderservice.dto.OrderLineItemsDto;
 import com.serdarfirlayis.orderservice.dto.OrderRequest;
+import com.serdarfirlayis.orderservice.event.OrderPlacedEvent;
 import com.serdarfirlayis.orderservice.model.Order;
 import com.serdarfirlayis.orderservice.model.OrderLineItems;
 import com.serdarfirlayis.orderservice.repository.OrderRepository;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,6 +29,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -61,6 +64,7 @@ public class OrderService {
 
             if (Boolean.TRUE.equals(allProductsInStock)) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order placed successfully";
             } else {
                 throw new IllegalArgumentException("Product is not in stock, please try again later");
